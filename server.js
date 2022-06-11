fc987fe665a2ecd2fd5b0c05aa15a55c91fb796d
murageh <muragehdeveloper@gmail.com>
2022-06-11 20:10:17 +0300

    modify server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
+
+server.db = router.db;
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
@@ -18,6 +21,7 @@ server.use((req, res, next) => {
   next();
 });
 
+server.use(auth);
 server.use(router);
 
 server.listen(port, "0.0.0.0", (err) => {
