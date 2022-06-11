18f6d545f8a41083424471de93d542a1403a40c2
murageh <muragehdeveloper@gmail.com>
2022-06-11 20:05:42 +0300

    modify server.js

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
+// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
+
+server.get("/ping", (req, res) => res.send("pong"));
+
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
 server.use(router);
 
 server.listen(port, "0.0.0.0", (err) => {
